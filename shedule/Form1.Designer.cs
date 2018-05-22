a23b385b05a6ae4c087bbf2a4fee05b3f2d88a6d
Денис Хрущев <squil@mail.ru>
2018-05-22 23:45:41 +0900

    измененияя

--- shedule/Form1.Designer.cs
+++ shedule/Form1.Designer.cs
@@ -29,22 +29,64 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 12);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Р.Киплинг \"Серые глаза - рассвет...\" (Перевод К. Симонова)",
+            "Серые глаза - рассвет,",
+            "Пароходная сирена,",
+            "Дождь, разлука, серый след",
+            "За винтом бегущей пены.",
+            "",
+            "Черные глаза - жара,",
+            "В море сонных звезд скольженье,",
+            "И у борта до утра",
+            "Поцелуев отраженье.",
+            "",
+            "Синие глаза - луна,",
+            "Вальса белое молчанье,",
+            "Ежедневная стена",
+            "Неизбежного прощанья.",
+            "",
+            "Карие глаза - песок,",
+            "Осень, волчья степь, охота,",
+            "Скачка, вся на волосок",
+            "От паденья и полета.",
+            "",
+            "Нет, я не судья для них,",
+            "Просто без суждений вздорных",
+            "Я четырежды должник",
+            "Синих, серых, карих, черных.",
+            "",
+            "Как четыре стороны",
+            "Одного того же света,",
+            "Я люблю - в том нет вины -",
+            "Все четыре этих цвета."});
+            this.listBox1.Location = new System.Drawing.Point(12, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(311, 277);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(736, 436);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +97,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- shedule/Form1.cs
+++ shedule/Form1.cs
@@ -16,5 +16,10 @@ namespace shedule
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
